146b9ca39028fa1a34e63f917b5bc87daa2fbfb2
Baris Ciftci <barishciftci@gmail.com>
2023-04-16 19:09:52 +0200

    Initial Commit

---
+++ SushiMenuApp/SushiMenuAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SushiMenuAppApp.swift
+//  SushiMenuApp
+//
+//  Created by Baris Ciftci on 16/04/2023.
+//
+
+import SwiftUI
+
+@main
+struct SushiMenuAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
